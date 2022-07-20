b139737782526dd653a2630982815c79202e04fd
MilesDAC <5collm30@solent.ac.uk>
2022-07-20 19:38:03 +0100

    Activate
    + Remote functionality
    + Phone functionality
    + Torch functionality

--- Assets/Custom+/Scripts/RecordPlayer.cs
+++ Assets/Custom+/Scripts/RecordPlayer.cs
@@ -7,18 +7,20 @@ public class RecordPlayer : MonoBehaviour
     private AudioSource myAudio;
     public AudioClip musicClip;
     public Transform record;
-    public float rotationRate;
+    public float originalRotationRate = 78;
+    private float currentRotationRate = 0;
 
     private void Start()
     {
         myAudio = transform.GetComponent<AudioSource>();
         myAudio.clip = musicClip;
         myAudio.Play();
+        currentRotationRate = originalRotationRate;
     }
     // Update is called once per frame
     void Update()
     {
-        record.Rotate(0, rotationRate*Time.deltaTime, 0);
+        record.Rotate(0, currentRotationRate*Time.deltaTime, 0);
     }
     public void ToggleRecordPlayer()
     {
@@ -27,10 +29,12 @@ public class RecordPlayer : MonoBehaviour
             if (myAudio.isPlaying)
             {
                 myAudio.Pause();
+                currentRotationRate = 0;
             }
             else
             {
                 myAudio.UnPause();
+                currentRotationRate = originalRotationRate;
             }
         }
     }
